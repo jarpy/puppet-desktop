20aec493d36e83df96fc2d9459ebb74e868069f3
Toby McLaughlin <toby@jarpy.net>
2019-01-08 09:41:47 +1100

    Fully prepare Emacs Prelude

--- modules/emacs/manifests/init.pp
+++ modules/emacs/manifests/init.pp
@@ -6,7 +6,26 @@ class emacs {
   vcsrepo { "/home/${me}/.emacs.d":
     ensure   => present,
     provider => git,
-    source   => 'https://github.com/jarpy/prelude',
+    source   => 'git@github.com:jarpy/prelude.git',
     user     => $me,
+    notify   => Exec['bootstrap emacs'],
+  }
+
+  file { "/tmp/bootstrap-emacs.el":
+    content => template("emacs/bootstrap-emacs.el.erb"),
+  }
+  ->
+  exec { "bootstrap emacs":
+    command     => "/usr/bin/emacs --batch --load /tmp/bootstrap-emacs.el",
+    unless      => "/bin/stat /home/${me}/.emacs.d/elpa/solarized-theme-*",
+    user        => $me,
+    environment => [ "HOME=/home/${me}/"],
+  }
+  ~>
+  exec { "setup emacs":
+    command     => "/usr/bin/emacs --batch --load /home/${me}/.emacs.d/init.el",
+    user        => $me,
+    environment => [ "HOME=/home/${me}/"],
+    refreshonly => true,
   }
 }
